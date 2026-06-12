dd40cf2d463fa160026378043f44b58a4af98fb5
jayant-dewangan-hub <jayantdewangan07@gmail.com>
2026-06-12 11:20:32 +0530

    Add simple game entry system

--- game.c
+++ game.c
@@ -3,44 +3,55 @@
 
 int main(void)
 {
-    string name = get_string("What is your name for game?");
+    // Taking player's name
+    string name = get_string("What is your name for game? ");
     printf("Hello %s\n", name);
-int age;
+
+    // Asking age until a valid age is entered
+    int age;
     do
     {
-     age = get_int("What is your current age?");
+        age = get_int("What is your current age? ");
     }
-    while (age<1);
+    while (age < 1);
 
-    if ( age < 13 )
+    // Checking the player's eligibility
+    if (age < 13)
     {
-    printf("you are not eligible for this game\n");
+        printf("You are not eligible for this game.\n");
     }
     else
     {
-        printf("you are eligible for this game\n");
-        char c = get_char("Do you want to buy a VIP membership?(y/n)\n");
-        if (c=='y')
+        printf("You are eligible for this game.\n");
+
+        // Asking for VIP membership choice
+        char c;
+        do
         {
-            printf("VIP unlocked\n");
+            c = get_char("Do you want to buy a VIP membership? (y/n): ");
         }
-     else if (c=='n')
-     {
-            printf("Regular entry\n");
-    }
-    int time;
-    do
-    {
-     time = get_int("How many hour do you want to play?\n");
-    }
-     while (time<0);
+        while (c != 'y' && c != 'n');
+        if (c == 'y')
+        {
+            printf("VIP unlocked.\n");
+        }
+        else if (c == 'n')
+        {
+            printf("Welcome to Regular entry\n");
+        }
 
-     for (int i = 1; i <= time; i++)
-     {
-     printf("Playing hour %i\n", i);
-     }
+        // Taking play time in hours
+        int time;
+        do
+        {
+            time = get_int("How many hours do you want to play?\n");
+        }
+        while (time <= 0);
 
-
+        // Displaying each hours of gameplay
+        for (int i = 1; i <= time; i++)
+        {
+            printf("Playing hour %i\n", i);
+        }
     }
-    }
-
+}
